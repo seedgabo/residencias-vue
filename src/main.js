e96a7c0f79ed5d5c2b04af08a69524879c2a1978
seedgabo <seedgabo@gmail.com>
2017-08-05 23:58:35 -0500

    social login

--- src/main.js
+++ src/main.js
@@ -9,64 +9,83 @@ import App from './App'
 import router from './router'
 import Vue2Filters from 'vue2-filters'
 import VueQuillEditor from 'vue-quill-editor'
-
+import FBSignInButton from 'vue-facebook-signin-button'
+import GSignInButton from 'vue-google-signin-button'
+import Chartkick from 'chartkick'
+import VueChartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+import 'fullcalendar'
+import '../node_modules/fullcalendar/dist/locale-all.js'
 require('vue2-animate/dist/vue2-animate.min.css')
-
-Vue.use(Vuetify)
-Vue.use(Vue2Filters)
-
 const moment = require('moment')
 require('moment/locale/es')
 moment.locale('es')
-Vue.use(require('vue-moment'), {
-  moment
-})
-Vue.use(VueQuillEditor)
 
-import 'fullcalendar'
-import '../node_modules/fullcalendar/dist/locale-all.js'
-Vue.use(require('vue-full-calendar'))
-
-import Chartkick from 'chartkick'
 Chartkick.configure({
-  language: "es"
-})
-import VueChartkick from 'vue-chartkick'
-import Chart from 'chart.js'
-// window.Highcharts = require('highcharts')
-Vue.use(VueChartkick, {
-  Chartkick
+    language: "es"
 })
 
-import Croppa from 'vue-croppa'
-Vue.use(Croppa)
 
+
+
+
+
+
+
+
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '796212907168839',
+        cookie: true, // enable cookies to allow the server to access the session
+        xfbml: true, // parse social plugins on this page
+        version: 'v2.8' // use graph api version 2.8
+    });
+};
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+Vue.use(Vuetify)
+Vue.use(Vue2Filters)
+Vue.use(FBSignInButton)
+Vue.use(GSignInButton)
+Vue.use(VueQuillEditor)
+Vue.use(VueChartkick, { Chartkick })
+Vue.use(require('vue-moment'), { moment })
+Vue.use(require('vue-full-calendar'))
 Vue.config.productionTip = false
+
 
 var api = require('./services/api.js');
 api.ready = new Promise(
-  (resolve, reject) => {
-    var user = window.localStorage.getItem('user');
-    if (user) {
-      api.user = JSON.parse(user);
-      var residence = window.localStorage.getItem('residence');
-      if (residence) {
-        api.residence = JSON.parse(residence);
-      }
-      var i18n = window.localStorage.getItem('i18n');
-      if (i18n) {
-        api.i18n = JSON.parse(i18n);
-      }
-      return resolve(api.user);
-    } else {
-      return reject();
+    (resolve, reject) => {
+        var user = window.localStorage.getItem('user');
+        if (user) {
+            api.user = JSON.parse(user);
+            var residence = window.localStorage.getItem('residence');
+            if (residence) {
+                api.residence = JSON.parse(residence);
+            }
+            var i18n = window.localStorage.getItem('i18n');
+            if (i18n) {
+                api.i18n = JSON.parse(i18n);
+            }
+            return resolve(api.user);
+        } else {
+            return reject();
+        }
     }
-  }
 )
 
 router.beforeEach((to, from, next) => {
-  document.title = (to.name) + " | Residencias Online"
-  next()
+    document.title = (to.name) + " | Residencias Online"
+    next()
 })
 
 
@@ -74,10 +93,10 @@ router.beforeEach((to, from, next) => {
 /* eslint-disable no-new */
 
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: {
-    App,
-  }
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: {
+        App,
+    }
 })
