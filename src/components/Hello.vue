0e3f3f183e2ba6daf1c26d77009353ee21699c2d
seedgabo <seedgabo@gmail.com>
2017-08-07 12:27:44 -0500

    login error habdler

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-
-    <v-carousel v-if="sliders.length >0">
-      <v-carousel-item v-for="slider in sliders" :key="slider.id" v-bind:src="slider.image.url">
+    <v-carousel v-if="sliders.length >1">
+      <v-carousel-item v-for="slider in sliders" :key="slider.id" v-bind:src="slider.image.url" transition="fade" reverseTransition="fade">
         <v-layout row wrap>
           <v-flex xs12>
             <h3 class="white--text">{{slider.name}}</h3>
@@ -13,17 +12,36 @@
         </v-layout>
       </v-carousel-item>
     </v-carousel>
+    <v-parallax class="my-parallax" :src="sliders[0].image.url" v-if="sliders.length === 1" height="270">
+      <v-layout column align-center justify-center>
+        <h3 class="white--text">{{sliders[0].name}}</h3>
+        <h5 class="white--text">{{sliders[0].text}}</h5>
+      </v-layout>
+    </v-parallax>
+    <div style="position:relative; z-index:1">
+      <v-btn fab absolute top left primary dark medium>
+        <v-icon>add</v-icon>
+      </v-btn>
+    </div>
     <v-layout class="pt-3 pl-2">
-      <v-flex xs12 sm9>
-        <v-card>
-          <div v-for="post in posts" :key="post.id">
-            <v-card-title>
-              <h3 class="headline">
+      <v-flex xs12 sm8>
+        <v-card class="text-xs-center">
+          <template v-for="post in posts">
+            <v-card-title :key="post.id" class="text-xs-center">
+              <div class="headline primary--text">
                 {{post.title}}
-              </h3>
+              </div>
             </v-card-title>
-            <v-card-text v-html="post.text.substring(0,100)"></v-card-text>
-          </div>
+            <v-card-text v-html="post.text.substring(0,100)" class="text-xs-justify">
+            </v-card-text>
+            <v-card-text class="text-xs-right">
+              <span class="headline-2 mr-4" v-if="post.user">{{post.user.name}}</span>
+              <span class="grey--text">
+                {{ post.created_at | moment('from')}}
+              </span>
+            </v-card-text>
+            <v-divider></v-divider>
+          </template>
           <v-card-actions>
             <v-btn full block flat :to="'posts'">
               {{api.trans('literals.view_resource')}} {{api.trans('literals.more')}}
@@ -76,7 +94,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.carousel {
-  height: 300px;
+.carousel,
+.my-parallax {
+  height: 270px;
 }
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,164 +1,145 @@
-<template>
-  <div class="login-page">
-    <v-container fluid>
-      <v-layout class="text-xs-center">
-        <v-flex>
-          <img class=" delay bounceIn animated" src="../assets/logo-completo.png" alt="" style="height:90px;">
-        </v-flex>
-      </v-layout>
-      <v-layout>
-        <v-flex class="hidden-xs"></v-flex>
-        <v-flex>
-          <v-card class="login-card mt-5" hover raised>
-            <v-card-title primary-title>
-              <div class="text-center" style="width:100%">
-                <h4 class="headline">
-                  Login
-                </h4>
-                <v-text-field name="username" v-model="username" label="username" prepend-icon="account_circle"> </v-text-field>
-
-                <v-text-field name="password" v-model="password" label="password" prepend-icon="lock" :append-icon="e1 ? 'visibility' : 'visibility_off'" :append-icon-cb="() => (e1 = !e1)" :type="e1 ? 'password' : 'text'">
-                </v-text-field>
-                <v-btn @click.native="doLogin()" :loading="loging" primary block v-bind:disabled="password.length <4 || username.length <4">
-                  Login
-                </v-btn>
-
-                <fb-signin-button :params="fbSignInParams" @success="onFacebookSignInSuccess" @error="onSignInError">
-                  <v-btn primary flat>
-                    <v-icon class="blue--text">fa-facebook-square</v-icon>
-                    &nbsp;&nbsp;Facebook
-                  </v-btn>
-                </fb-signin-button>
-
-                <g-signin-button :params="googleSignInParams" @success="onGoogleSignInSuccess" @error="onSignInError">
-                  <v-btn danger flat>
-                    <v-icon class="red--text">fa-google-plus-square</v-icon>
-                    <span class="red--text">
-                      &nbsp;&nbsp;Google
-                    </span>
-                  </v-btn>
-                </g-signin-button>
-
-              </div>
-            </v-card-title>
-          </v-card>
-        </v-flex>
-        <v-flex class=" hidden-xs"></v-flex>
-      </v-layout>
-    </v-container>
-  </div>
+<template lang="jade">
+.login-page
+	v-container(fluid='')
+		v-layout.text-xs-center
+			v-flex
+				img.delay.bounceIn.animated(src='../assets/logo-completo.png', alt='', style='height:90px;')
+		v-layout
+			v-flex.hidden-xs
+			v-flex
+				v-card.login-card.mt-5(hover='', raised='')
+					v-card-title(primary-title='')
+						.text-center(style='width:100%')
+							h4.headline
+								| Login
+							v-text-field(name='username', v-model='username', label='username', prepend-icon='account_circle')
+							v-text-field(name='password', v-model='password', label='password', prepend-icon='lock', :append-icon="e1 ? 'visibility' : 'visibility_off'", :append-icon-cb='() => (e1 = !e1)', :type="e1 ? 'password' : 'text'")
+							v-btn(@click.native='doLogin()', :loading='loging', primary='', block='', v-bind:disabled='password.length <4 || username.length <4')
+								| Login
+							fb-signin-button(:params='fbSignInParams', @success='onFacebookSignInSuccess', @error='onSignInError')
+								v-btn(primary='', flat='')
+									v-icon.blue--text fa-facebook-square
+									|                       Facebook
+							g-signin-button(:params='googleSignInParams', @success='onGoogleSignInSuccess', @error='onSignInError')
+								v-btn(danger='', flat='')
+									v-icon.red--text fa-google-plus-square
+									span.red--text
+										| Google
+			v-flex.hidden-xs
+	v-dialog(v-model="error")
+		v-card
+			v-card-title.subheader Error
+			v-card-text: p {{errorText}}
+			v-card-actions
+				v-spacer
+				v-btn(flat primary dark, @click="error=false") {{api.trans('literals.ok')}}
 </template>
 
-<script>
-const axios = require('axios')
-export default {
-  name: 'login',
-  data() {
-    return {
-      api: require('../services/api.js'),
-      username: 'seedgabo@gmail.com ',
-      password: '',
-      e1: true,
-      loging: false,
-      fbSignInParams: {
-        scope: 'email,public_profile,user_birthday',
-        return_scopes: true
-      },
-      googleSignInParams: {
-        client_id: '425679220353-u39prig4hkrjg592lnppmnbfj6lvi4qk.apps.googleusercontent.com'
-      }
-    }
-  },
-  methods: {
-    doLogin() {
-      this.loging = true;
-      axios.get(this.api.url + 'login', {
-        headers: { 'Authorization': "Basic " + btoa(this.username + ':' + this.password) }
-      })
-        .then((response) => {
-          this.api.ready = new Promise((resolve, reject) => {
-            console.log(response.data);
-            this.loging = false;
-            this.api.setUser(response.data)
-            window.localStorage.setItem('user', JSON.stringify(response.data.user));
-            window.localStorage.setItem('residence', JSON.stringify(response.data.residence));
-            this.getLangs();
-            resolve(response.data)
-            this.$router.push('/');
-            this.$router.app.$emit('login', response.data)
-          })
-        })
-        .catch((err) => {
-          console.error(err);
-        })
-    },
-    getLangs() {
-      this.api.get('lang')
-        .then((response) => {
-          console.log(response.data);
-          this.api.i18n = response.data;
-          window.localStorage.setItem('i18n', JSON.stringify(response.data));
-        })
-        .catch(console.error)
-    },
-    onFacebookSignInSuccess(response) {
-      FB.api('/me', { fields: 'name,birthday,email,link,gender,picture' }, (fuser) => {
-        console.log(fuser)
-        this.SocialLogin(fuser);
-        console.log(`Good to see you, ${fuser.name}.`)
-      })
-    },
-    onGoogleSignInSuccess(googleUser) {
-      const profile = googleUser.getBasicProfile()
-      console.log(profile);
-      var user = {
-        name: profile.getName(),
-        image: profile.getImageUrl(),
-        email: profile.getEmail(),
-        google_id: profile.getId(),
-      }
-      this.SocialLogin(user)
-    },
-    onSignInError(error) {
-      console.log('OH NOES', error)
-    },
-    SocialLogin(user) {
-      this.loging = true;
-      axios.post(this.api.url + 'login/oauth', { email: user.email, user: user })
-        .then((response) => {
-          this.api.ready = new Promise((resolve, reject) => {
-            console.log(response.data);
-            this.loging = false;
-            this.api.setUser(response.data)
-            window.localStorage.setItem('user', JSON.stringify(response.data.user));
-            window.localStorage.setItem('residence', JSON.stringify(response.data.residence));
-            this.getLangs();
-            resolve(response.data)
-            this.$router.push('/');
-            this.$router.app.$emit('login', response.data)
-          })
-        })
-        .catch((err) => {
-          console.error(err);
-        })
-    },
-  }
-}
+<script lang="coffee">
+axios = require('axios')
+module.exports =
+	name: 'login'
+	data: ->
+		api: require '../services/api.js'
+		username:'seedgabo@gmail.com '
+		password:''
+		e1:true
+		loging:false
+		error:false
+		errorText: "Error"
+		fbSignInParams:
+			scope: 'email,public_profile,user_birthday'
+			return_scopes: true
+		googleSignInParams:
+			client_id:'425679220353-u39prig4hkrjg592lnppmnbfj6lvi4qk.apps.googleusercontent.com'
+	methods:
+		doLogin: ()->
+			@loging = true;
+			axios.get(@api.url + 'login', {
+				headers: { 'Authorization': "Basic " + btoa(@username + ':' + @password) }
+			})
+			.then (response) =>
+					@api.ready = new Promise (resolve, reject) =>
+						console.log(response.data);
+						@loging = false;
+						@api.setUser(response.data)
+						window.localStorage.setItem('user', JSON.stringify(response.data.user));
+						window.localStorage.setItem('residence', JSON.stringify(response.data.residence));
+						@getLangs();
+						resolve(response.data)
+						@$router.push('/');
+						@$router.app.$emit('login', response.data)
+			.catch (err) =>
+				console.error err
+				@onError(err)
+		getLangs: ()->
+			@api.get('lang')
+				.then (response) =>
+					console.log(response.data);
+					@api.i18n = response.data;
+					window.localStorage.setItem('i18n', JSON.stringify(response.data));
+				.catch console.error
+		onFacebookSignInSuccess: (response)->
+			FB.api '/me',{ fields: 'name,birthday,email,link,gender,picture' },(fuser)=>
+				console.log(fuser)
+				@SocialLogin(fuser,'facebook');
+				console.log("""Good to see you, ${fuser.name}.""")
+		onGoogleSignInSuccess: (googleUser)->
+			profile = googleUser.getBasicProfile()
+			console.log(profile)
+			user=
+				name: profile.getName()
+				image: profile.getImageUrl()
+				email: profile.getEmail()
+				google_id: profile.getId()
+			@SocialLogin(user,'google')
+		onSignInError: (error)->
+			console.log('OH NOES', error)
+			@error=true
+			@errorText="Error en Login"
+		onError: (resp,social)->
+			console.log resp.response
+			@loging=false
+			@error=true
+			if (social=='facebook'||social=='google')&&resp.response.status==401
+				@errorText="Error en autenticación, el usuario no esta registrado"
+			else if resp.response.status==401
+				@errorText="Usuario y/o contraseña Invalidos"
+			else
+				@errorText="Error en autenticación: "+resp.response.statusText
+		SocialLogin: (user,social)->
+			@loging = true
+			axios.post(@api.url + 'login/oauth', { email: user.email, user: user })
+				.then (response) =>
+					@api.ready = new Promise (resolve, reject)=>
+						console.log(response.data)
+						@loging = false
+						@api.setUser(response.data)
+						window.localStorage.setItem('user', JSON.stringify(response.data.user))
+						window.localStorage.setItem('residence', JSON.stringify(response.data.residence))
+						@getLangs();
+						resolve(response.data)
+						@$router.push('/')
+						@$router.app.$emit('login', response.data)
+				.catch (err) =>
+					console.error(err)
+					@onError(err,social)
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .login-page,
 body {
-  background: #fff;
-  margin-top: 0px;
-  height: 100vh;
-  width: 100vw;
-  background-image: url(https://s-media-cache-ak0.pinimg.com/originals/d2/b8/fc/d2b8fc669367ac5eac78f9f2d4186913.png);
-  background-size: cover;
+	background: #fff;
+	margin-top: 0px;
+	height: 100vh;
+	width: 100vw;
+	background-image: url(https://s-media-cache-ak0.pinimg.com/originals/d2/b8/fc/d2b8fc669367ac5eac78f9f2d4186913.png);
+	background-size: cover;
 }
 
 .delay {
-  animation-delay: 1s;
+	animation-delay: 1s;
 }
 </style>
